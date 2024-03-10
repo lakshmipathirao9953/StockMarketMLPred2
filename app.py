a18284e8a2c0deb894e3003bca5340ad3905c636
lakshmipathirao9953 <lakshmipathirao9953@gmail.com>
2024-03-10 07:50:06 +0530

    initial commit

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model('C:\\Users\\Lakshmipathi Rao\\Desktop\\Stock\\Stock market predictions model.keras')
-
+try:
+    model = load_model('C:\Users\Lakshmipathi Rao\Desktop\Stock\Stock market predictions model.keras')
+except Exception as e:
+    st.error(f"Error loading the model: {e}")
 
 st.header('Stock Market Predictor')
 
-stock =st.text_input('Enter Stock Symnbol', 'GOOG')
+stock = st.text_input('Enter Stock Symbol', 'GOOG')
 start = '2012-01-01'
 end = '2022-12-31'
 
-data = yf.download(stock, start ,end)
+data = yf.download(stock, start, end)
 
 st.subheader('Stock Data')
 st.write(data)
@@ -23,7 +25,7 @@ data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
 data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
 
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range=(0,1))
+sc = MinMaxScaler(feature_range=(0, 1))
 
 past_100_days_of_train_data = data_train.tail(100)
 data_test = pd.concat([past_100_days_of_train_data, data_test], ignore_index=True)
@@ -31,7 +33,7 @@ data_test_scale = sc.fit_transform(data_test)
 
 st.subheader('Price vs MA50')
 ma_50_days = data.Close.rolling(50).mean()
-fig1 = plt.figure(figsize=(8,6))
+fig1 = plt.figure(figsize=(8, 6))
 plt.plot(ma_50_days, 'r')
 plt.plot(data.Close, 'g')
 plt.show()
@@ -39,7 +41,7 @@ st.pyplot(fig1)
 
 st.subheader('Price vs MA50 vs MA100')
 ma_100_days = data.Close.rolling(100).mean()
-fig2 = plt.figure(figsize=(8,6))
+fig2 = plt.figure(figsize=(8, 6))
 plt.plot(ma_50_days, 'r')
 plt.plot(ma_100_days, 'b')
 plt.plot(data.Close, 'g')
@@ -48,7 +50,7 @@ st.pyplot(fig2)
 
 st.subheader('Price vs MA100 vs MA200')
 ma_200_days = data.Close.rolling(200).mean()
-fig3 = plt.figure(figsize=(8,6))
+fig3 = plt.figure(figsize=(8, 6))
 plt.plot(ma_100_days, 'r')
 plt.plot(ma_200_days, 'b')
 plt.plot(data.Close, 'g')
@@ -60,22 +62,23 @@ y = []
 
 for i in range(100, data_test_scale.shape[0]):
     x.append(data_test_scale[i-100:i])
-    y.append(data_test_scale[i,0])
+    y.append(data_test_scale[i, 0])
 
-x,y = np.array(x), np.array(y)
+x, y = np.array(x), np.array(y)
 
-pred = model.predict(x)
+if 'model' in locals():
+    pred = model.predict(x)
+    scale = 1 / sc.scale_
+    pred = pred * scale
+    y = y * scale
 
-scale = 1/sc.scale_
-
-pred = pred * scale
-y = y * scale
-
-st.subheader('Original Price vs Predicted Price')
-fig4 = plt.figure(figsize=(8,6))
-plt.plot(pred, 'r', label='Original Price')
-plt.plot(y, 'g', label = 'Predicted Price')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.show()
-st.pyplot(fig4)
+    st.subheader('Original Price vs Predicted Price')
+    fig4 = plt.figure(figsize=(8, 6))
+    plt.plot(pred, 'r', label='Original Price')
+    plt.plot(y, 'g', label='Predicted Price')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.show()
+    st.pyplot(fig4)
+else:
+    st.warning("Model couldn't be loaded. Please check the model file and try again.")
